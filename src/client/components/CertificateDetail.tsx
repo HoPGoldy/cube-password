147335e57cf993179e1339dc916cdeea94038740
hopgoldy <hpgless@outlook.com>
2022-08-01 23:35:08 +0800

    feat: 完善凭证详情

--- src/client/components/CertificateDetail.tsx
+++ src/client/components/CertificateDetail.tsx
@@ -1,36 +1,68 @@
-import { Delete, AddO } from '@react-vant/icons'
-import React, { FC, useContext } from 'react'
-import { Form, Overlay } from 'react-vant'
+import React, { FC, useContext, useRef, useState } from 'react'
+import { Form, Overlay, Dialog, hooks } from 'react-vant'
+import { Question, Plus } from '@react-vant/icons'
 import { Button } from '@/client/components/Button'
 import { AppConfigContext } from './AppConfigProvider'
 import CertificateFieldItem from './CertificateFieldItem'
+import { CertificateField } from '@/types/app'
+
+const { useClickAway } = hooks
 
 interface Props {
     visible: boolean
     onClose: () => void
 }
 
+const defaultFields: CertificateField[] = [
+    { label: '密码', value: '' },
+    { label: '网址', value: '' }
+]
+
 const CertificateDetail: FC<Props> = (props) => {
     const { visible, onClose } = props
     const [config] = useContext(AppConfigContext)
+    const [title, setTitle] = useState('新密码')
+    const [form] = Form.useForm()
+    const newFieldIndex = useRef(1)
+    const dialogRef = useRef<HTMLDivElement>(null)
 
-    const onFinish = (values: any) => {
-        console.log(values)
+    const onFinish = () => {
+        console.log(form.getFieldsValue())
     }
 
+    const onConfirmClose = async () => {
+        await Dialog.confirm({
+            message: '确定要关闭吗？未保存的内容都将丢失',
+            confirmButtonText: '关闭',
+            confirmButtonColor: '#ef4444'
+        })
+        onClose()
+    }
+
+    useClickAway(dialogRef, (e) => {
+        // 由于点击显示弹窗的按钮也会触发 useClickAway
+        // 所以这里要判断下只有点击 div 元素时才会触发关闭确认
+        if ((e.target as HTMLDivElement).tagName !== 'DIV') return
+        onConfirmClose()
+    })
+
     return (
-        <Overlay visible={visible} duration={100}>
-            <div className='overflow-y-auto h-screen flex items-center justify-center'>
-                <div className='bg-slate-200 p-4 mx-4 rounded-lg w-[60vw] my-10'>
+        <Overlay visible={visible} duration={100} className='overflow-y-auto'>
+            <div className='flex justify-center'>
+                <div ref={dialogRef} className='bg-slate-200 relative p-4 mx-4 my-10 rounded-lg w-screen md:w-[80vw] lg:w-[60vw] xl:w-[40%]'>
                     <input
                         type="text"
-                        value='新密码'
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
                         className='font-bold text-xl bg-inherit mb-4'
                     />
-                    <Form onFinish={onFinish} className='rounded-lg bg-white pt-4 flex flex-row flex-wrap'>
-                        <Form.List name="fields" initialValue={[{ label: '密码', value: '123321' }]}>
-                            {(fields, { add, remove }) => (
-                                <>
+                    <div className='absolute cursor-default top-5 right-5 flex items-center text-gray-500'>
+                        <Question className='mr-2' /> 标题名和字段名均可修改
+                    </div>
+                    <Form form={form} className='rounded-lg bg-white pt-4'>
+                        <Form.List name="fields" initialValue={defaultFields}>
+                            {(fields, { add, remove }) => (<>
+                                <div className='flex flex-row flex-wrap'>
                                     {fields.map((field, idx) => {
                                         return (
                                             <Form.Item
@@ -38,25 +70,35 @@ const CertificateDetail: FC<Props> = (props) => {
                                                 customField
                                                 name={[field.name]}
                                             >
-                                                <CertificateFieldItem widthClass={idx === 0 ? 'w-full' : 'w-[50%]'} />
+                                                <CertificateFieldItem
+                                                    widthClass={idx === 0 ? 'w-full' : 'w-full md:w-[50%]'}
+                                                    showDelete={fields.length > 1}
+                                                    onDelete={() => remove(idx)}
+                                                />
                                             </Form.Item>
                                         )
                                     })}
-                                    <div className='mx-4 pt-2 pb-4'>
-                                        <Button plain className='!border-slate-300' block icon={<AddO />} onClick={() => add({ label: '字段' + fields.length, value: '' })}>
-                                            新增字段
-                                        </Button>
-                                    </div>
-                                </>
-                            )}
+                                </div>
+                                <div className='mx-4 pt-4 pb-6'>
+                                    <Button
+                                        plain
+                                        className='!border-slate-300'
+                                        block
+                                        icon={<Plus />}
+                                        onClick={() => add({ label: '字段' + newFieldIndex.current++, value: '' })}
+                                    >
+                                        新增字段
+                                    </Button>
+                                </div>
+                            </>)}
                         </Form.List>
                     </Form>
 
-                    <div className='flex flex-col md:flex-row'>
-                        <Button className='!mt-4 md:!mr-4 md:w-[50%]' block onClick={onClose}>
-                            放弃
+                    <div className='flex flex-row justify-between'>
+                        <Button className='!mt-4 md:!mr-4 !w-[20%] md:!w-[50%]' onClick={onConfirmClose}>
+                            返回
                         </Button>
-                        <Button className='!mt-4 md:w-[50%]' color={config?.buttonColor} block>
+                        <Button className='!mt-4 !w-[75%] md:!w-[50%]' color={config?.buttonColor} onClick={onFinish}>
                             提交
                         </Button>
                     </div>
--- src/client/components/CertificateFieldItem.tsx
+++ src/client/components/CertificateFieldItem.tsx
@@ -1,45 +1,69 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { CertificateField } from '@/types/app'
+import { Clear, Eye, ClosedEye } from '@react-vant/icons'
 
 
 interface Props {
     widthClass?: string
+    showDelete?: boolean
     value?: CertificateField
     onChange?: (value: CertificateField) => void
+    onDelete?: () => void
 }
 
 const CertificateFieldItem: FC<Props> = (props) => {
-    const { widthClass, value, onChange } = props
+    const { widthClass, value, onChange, showDelete = true, onDelete } = props
+    const [hiddenPassword, setHiddenPassword] = useState(true)
 
     const isPassword = !!['密码', 'password'].find(text => value?.label.includes(text))
-    console.log('isPassword', isPassword)
 
     const onLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = { ...value, label: e.target.value }
         onChange?.(newValue as CertificateField)
     }
 
-    const onValueChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const onValueChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const newValue = { ...value, value: e.target.value }
         onChange?.(newValue as CertificateField)
     }
 
     return (
-        <div className={'px-4 pb-4 ' + widthClass}>
+        <div className={'px-4 pb-4 relative ' + widthClass}>
             <input
                 type="text"
                 value={value?.label}
                 onInput={onLabelChange}
                 className='mb-2 w-full'
             />
-            <textarea
-                value={value?.value}
-                onInput={onValueChange}
-                rows={1}
-                className='block px-3 py-2 w-full min-h-[42px] 
-                    border border-slate-300 rounded-md shadow-sm placeholder-slate-400
-                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
-            />
+            {
+                isPassword ?
+                    <input
+                        type={hiddenPassword ? 'password' : 'text'}
+                        value={value?.value}
+                        onChange={onValueChange}
+                        className='block px-3 py-2 w-full min-h-[42px] 
+                            border border-slate-300 rounded-md shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+                    /> :
+                    <textarea
+                        value={value?.value}
+                        onChange={onValueChange}
+                        rows={1}
+                        className='block px-3 py-2 w-full min-h-[42px] 
+                            border border-slate-300 rounded-md shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+                    />
+            }
+            {isPassword && (hiddenPassword ?
+                <Eye className='absolute right-14 top-11 cursor-pointer' fontSize={18} onClick={() => setHiddenPassword(false)} /> :
+                <ClosedEye className='absolute right-14 top-11 cursor-pointer' fontSize={18} onClick={() => setHiddenPassword(true)} />
+            )}
+
+            {showDelete && <Clear
+                className='absolute right-6 top-11 cursor-pointer text-red-400'
+                fontSize={18}
+                onClick={onDelete}
+            />}
         </div>
     )
 }
